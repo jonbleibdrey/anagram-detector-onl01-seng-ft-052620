ff8902f21386d3ff5c3292bcf54f88acfd02fbc2
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-06-28 19:26:58 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,14 +1,50 @@
 # Your code goes here!
-class Anagram
-  def initialize(string)
-    @string = string
+
+class Anagram 
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
   end
   
-  def anagram?(string)
-    string.chars.sort == @string.chars.sort
+  def match(string)
+    string.select do |w|
+      w.split("").sort == word.split("").sort
+    end
+    
   end
   
-  def match(to_match)
-    to_match.select {|str| anagram? str}
-  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Anagram
+#   def initialize(string)
+#     @string = string
+#   end
+  
+#   def anagram?(string)
+#     string.chars.sort == @string.chars.sort
+#   end
+  
+#   def match(to_match)
+#     to_match.select {|str| anagram? str}
+#   end
+# end
